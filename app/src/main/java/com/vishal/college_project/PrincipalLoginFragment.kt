59456cdbf5b99f7b609da38e1808fb61c0f603b5
panditvishal04 <panditvishal9546@gmail.com>
2023-08-19 12:39:28 +0530

    Initial commit

---
+++ app/src/main/java/com/vishal/college_project/PrincipalLoginFragment.kt
@@ -0,0 +1,40 @@
+package com.vishal.college_project
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.vishal.college_project.databinding.FragmentPrincipalLoginPageBinding
+
+class PrincipalLoginFragment : Fragment() {
+    lateinit var binding: FragmentPrincipalLoginPageBinding
+    lateinit var mainActivity: MainActivity
+    lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainActivity = activity as MainActivity
+        navController = findNavController()
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentPrincipalLoginPageBinding.inflate(layoutInflater)
+        return  binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
+}
